b9b45b401a3f548f7f6389efe71710a17cc7e33e
Oksana-999 <ksuha_077@mail.ru>
2015-12-12 12:55:40 +0200

    Update and rename www/autoload.php to WWW/autoload.php

--- WWW/autoload.php
+++ WWW/autoload.php
@@ -3,7 +3,6 @@ function __autoload($className)
 {
     if (file_exists(__DIR__ . '/controllers/' . $className . '.php'))
     {
-        //echo __DIR__ . '/controllers/' . $className . '.php';die;
         require __DIR__ . '/controllers/' . $className . '.php';
     }
     elseif (file_exists(__DIR__ . '/models/' . $className . '.php'))
